3d60e5bac3f4eff488a30d4fe174f38c59ad25e3
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-06 20:59:25 +0900

    3 の倍数あるいは 3 の数字が含まれる場合のチェックを追加

--- javaExercise/src/Ex1NumberDisplay.java
+++ javaExercise/src/Ex1NumberDisplay.java
@@ -1,9 +1,26 @@
 public class Ex1NumberDisplay {
 
+	/** 10進数字 n に 3 が含まれていれば true */
+	private boolean isThere3(int n) {
+		while(n > 0){
+			if( (n % 10) == 3) {
+				return true;
+			}else{
+				n /= 10;
+			}
+		}
+		return false;
+	}
 	//---------------- とりあえず 1 から 100 まで半角数字で出力
+	//---------------- 3 の倍数か 3 の数字が含まれていたら *** を出力
 	public void go(){
 		for(int i=0; i<100; i++){
-			System.out.println(i+1);
+			if( ((i+1) % 3) == 0 || isThere3(i+1) ){
+				System.out.print("*** ");
+				System.out.println(i+1);
+			}else{
+				System.out.println(i+1);
+			}
 		}
 	}
 
