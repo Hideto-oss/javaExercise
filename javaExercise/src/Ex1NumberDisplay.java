c283741a0cff0899bc78592b90cbd73a4babbd15
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-06 20:30:57 +0900

    暫定的に 1 から 100 まで半角数字で出力する。

---
+++ javaExercise/src/Ex1NumberDisplay.java
@@ -0,0 +1,17 @@
+public class Ex1NumberDisplay {
+
+	//---------------- とりあえず 1 から 100 まで半角数字で出力
+	public void go(){
+		for(int i=0; i<100; i++){
+			System.out.println(i+1);
+		}
+	}
+
+	//課題 1.（世界のナベアツ）
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ex1NumberDisplay nd = new Ex1NumberDisplay();
+		nd.go();
+	}
+
+}
