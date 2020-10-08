1627ef5a56683bb821b57094e17ea8204898f8f7
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-08 19:30:35 +0900

    次の課題と雛型の class を追加

---
+++ javaExercise/src/Ex2BigNumCalc.java
@@ -0,0 +1,20 @@
+
+public class Ex2BigNumCalc {
+
+	/** 桁数の上限を定義 */
+	private final int DIGIT_LIMIT = 25;
+
+	public void go(){
+		//ここに処理を書く（メソッドなどは自由に追加の事）
+		System.out.println("Not Yet");	//Dummy
+	}
+
+	//課題 2.（巨大数の和と差）
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ex2BigNumCalc bnc = new Ex2BigNumCalc();
+		bnc.go();
+
+	}
+
+}
