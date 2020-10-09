f9f9eae237a9c9109ec0ceb0572459f6141f4a82
Haruto-oss <haruto@pluto.sakura.ne.jp>
2020-10-09 21:11:12 +0900

    和の処理だけ実装しました。

--- javaExercise/src/Ex2BigNumCalc.java
+++ javaExercise/src/Ex2BigNumCalc.java
@@ -3,14 +3,32 @@ public class Ex2BigNumCalc {
 	/** 桁数の上限を定義 */
 	private final int DIGIT_LIMIT = 25;
 
-	/** 文字列の数字を受け取り数値、にして char 配列で返す */
+	/** 文字列が数字であるかどうかをチェック */
+	private boolean isOk(String numstr) {
+		for(int i = 0; i < numstr.length(); i++ ) {
+			if( !Character.isDigit( numstr.charAt(i)) ) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/** size の char[] を new し 0 クリアして返す */
+	private char[] getZeroCharArray( int size) {
+		char[] array;
+		array = new char[size];
+		// zero clear
+		for(int i = 0; i <size; i++) {
+			array[i] = 0;
+		}
+		return array;
+	}
+
+	/** 文字列の数字を受け取り、数値にして char 配列で返す */
 	private char[] toCharArray( String numstr) {
 
-		char[] ret = new char[DIGIT_LIMIT];
-		// zero clear
-		for(int i=0; i<DIGIT_LIMIT; i++) {
-			ret[i] = 0;
-		}
+		char[] ret = getZeroCharArray(DIGIT_LIMIT);
+
 		//右詰めで格納するためスタートポジションを計算
 		int length = numstr.length();
 		int start_pos = DIGIT_LIMIT - length;
@@ -26,13 +44,47 @@ public class Ex2BigNumCalc {
 		return ret;
 	}
 
+	/** n1, n2 の数値を足して、答えを char[] で返す*/
+	private char[] plus( String n1, String n2) {
+		if( !isOk(n1) || !isOk(n2) ) {
+			System.out.println("Cannot calcuration. Not digit.");
+			System.exit(1);
+		}
+		char[] answer = getZeroCharArray(DIGIT_LIMIT + 1);
+		char[] n1Array = toCharArray(n1);
+		char[] n2Array = toCharArray(n2);
+		char  carry = 0;
+		for( int i = DIGIT_LIMIT-1; i >= 0; i-- ) {
+			answer[i+1] = (char)(n1Array[i] + n2Array[i] + carry);
+			carry = (char)((int)answer[i+1] / 10);
+			answer[i+1] = (char)((int)answer[i+1] % 10);
+		}
+		answer[0] = carry;
+		return answer;
+	}
+
 	/** char 配列の数値を表示する(マイナスも考慮) */
 	private void printBigNumber( char[] numAry, boolean isMinus) {
+
+		//--------------------------- ゼロだった場合は 0 を表示し return
+		boolean all_zero = true;
+		for( int i = 0; i < numAry.length; i++ ) {
+			if( numAry[i] != 0 ) {
+				all_zero = false;
+				break;
+			}
+		}
+		if( all_zero ) {
+			System.out.println("0");
+			return;
+		}
+
+		//--------------------------- それ以外の場合の処理
 		boolean start_zero = false;
 		if(isMinus) {
 			System.out.print("-");
 		}
-		for(int i = 0; i < DIGIT_LIMIT; i++ ) {
+		for(int i = 0; i < numAry.length; i++ ) {
 			if( i == 0 && numAry[i] == 0 ) {
 				start_zero = true;
 				continue;
@@ -60,11 +112,24 @@ public class Ex2BigNumCalc {
 		printBigNumber( toCharArray(test3), true  );
 		printBigNumber( toCharArray(test4), true  );
 		printBigNumber( toCharArray(test5), false );
+
+		System.out.println(isOk("0123456789"));	//true
+		System.out.println(isOk("012A456789"));	//false
+		System.out.println(isOk("012BCDEF89"));	//false
+		System.out.println(isOk("987654321234567"));	//true
 	}
+
 	public void go(){
 		//ここに処理を書く（メソッドなどは自由に追加の事）
 		System.out.println("Not Yet");	//Dummy
-		checkMethods();
+		checkMethods();						//Dalete later
+		printBigNumber( plus("123456","987654"), false );		// 1111110
+		printBigNumber( plus("1234567890123456789012345",
+										"9876543210987654321098765"), false );
+									//11111111101111111110111110
+		printBigNumber( plus("0","0"), false );		// 0
+		printBigNumber( plus("0","A"), false );		// Error!
+
 	}
 
 	//課題 2.（巨大数の和と差）
