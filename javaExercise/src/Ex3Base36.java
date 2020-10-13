6535d4cb0e6f5b56165967ef13c99b5febbaa9b2
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-13 20:45:19 +0900

    さて、次の課題でっせ。

---
+++ javaExercise/src/Ex3Base36.java
@@ -0,0 +1,14 @@
+
+public class Ex3Base36 {
+
+	public void go(){
+		//処理を書く
+		System.out.println("Not yet. Sorry.");
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ex3Base36 b36 = new Ex3Base36();
+		b36.go();
+	}
+
+}
