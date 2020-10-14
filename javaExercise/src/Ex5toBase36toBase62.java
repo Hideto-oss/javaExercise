c4e6591840774b9b049c63481903b0818b15fa1d
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-14 21:28:53 +0900

    今度は逆変換の課題です。

---
+++ javaExercise/src/Ex5toBase36toBase62.java
@@ -0,0 +1,15 @@
+
+public class Ex5toBase36toBase62 {
+
+	public void go(){
+		//処理を書く
+		System.out.println("Not yet. Sorry.");
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ex5toBase36toBase62 conv = new Ex5toBase36toBase62();
+		conv.go();
+
+	}
+
+}
