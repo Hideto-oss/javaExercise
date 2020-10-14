b6bbb151bb832ab0762809d90a5ba4409f65ac1b
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-14 18:53:27 +0900

    さて次の課題ですよ

---
+++ javaExercise/src/Ex4Base62.java
@@ -0,0 +1,14 @@
+
+public class Ex4Base62 {
+
+	public void go(){
+		//処理を書く
+		System.out.println("Not yet. Sorry.");
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ex4Base62 b62 = new Ex4Base62();
+		b62.go();
+	}
+
+}
