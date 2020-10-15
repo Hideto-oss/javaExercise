41f0ce34110c8ba24e5b9c677b375b14b162115e
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-15 18:27:34 +0900

    次は素因数分解の課題です。

---
+++ javaExercise/src/Ex6PrimeNumbers.java
@@ -0,0 +1,15 @@
+
+public class Ex6PrimeNumbers {
+
+	public void go(){
+		//処理を書く
+		System.out.println("Not yet. Sorry.");
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ex6PrimeNumbers pn = new Ex6PrimeNumbers();
+		pn.go();
+	}
+
+}
