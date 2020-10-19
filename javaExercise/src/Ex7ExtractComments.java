01e5827711520f7803d3d2db1ab0e3018e6de91e
Hideto-oss <hideto_h@pluto.sakura.ne.jp>
2020-10-19 17:42:21 +0900

    次の課題はコメント取り出しです

---
+++ javaExercise/src/Ex7ExtractComments.java
@@ -0,0 +1,13 @@
+public class Ex7ExtractComments {
+
+	public void go(){
+		//処理を書く
+		System.out.println("Not yet. Sorry.");
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ex7ExtractComments ec = new Ex7ExtractComments();
+		ec.go();
+	}
+}
