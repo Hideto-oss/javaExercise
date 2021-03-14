f7096193df3970db7a242964a44e726865385dcd
Kaduki-oss <kaduki@pluto.sakura.ne.jp>
2021-03-14 21:25:45 +0900

    ゲームらしくなってきたぞ commit

--- javaExercise/src/Ex10flag/Main.java
+++ javaExercise/src/Ex10flag/Main.java
@@ -3,6 +3,7 @@ package Ex10flag;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.ImagePattern;
@@ -12,28 +13,69 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	BorderPane root;
-
-	//国旗の幅
-	private double WIDTH = 207.0;
-	//国旗の高さ
-	private double HEIGHT = 138.0;
-
 	Pane flagBase;
+	private Countries cFlag;
+
+	private final static String[] items = {
+			"国名を選択して下さい",			//000
+			"アフガニスタン",					//001
+			"アルバニア",							//002
+			"アルジェリア",						//003
+			"アンドラ公国",						//004
+			"アンゴラ共和国",					//005
+			"南極大陸(Antarctica)",			//006
+			"アンティグア・バーブーダ",		//007
+			"アルゼンチン",						//008
+			"アルメニア",							//009
+			"アルバ",								//010
+			"オーストラリア",					//011
+			"オーストリア",						//012
+			"アゼルバイジャン",					//013
+			"バハマ",								//014
+			"バーレーン",							//015
+			"バングラデシュ"						//016
+	};
 
 	@Override
 	public void start(Stage pstage) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
-		Button decideButton = new Button("決定");
-		decideButton.setOnAction((event)->{
-			makeFlag(flagBase);
-		});
 
+		// TODO 自動生成されたメソッド・スタブ
 		flagBase = new Pane();
 		flagBase.setPrefSize(Countries.WIDTH, Countries.HEIGHT);
 		makeFlag(flagBase);
+
+		ComboBox<String> flagCombo = new ComboBox<String>();
+		for(int i=0; i<items.length; i++) {
+			flagCombo.getItems().add(i,items[i]);
+		}
+		flagCombo.setValue(flagCombo.getItems().get(0));
+
+		Button decideButton = new Button("決定");
+		decideButton.setOnAction((event)->{
+			int selectNum = flagCombo.getSelectionModel().getSelectedIndex();
+			if(selectNum == 0 || selectNum == -1) {
+				System.out.println("コンボボックスで国名を選択して下さい。");
+			}else {
+				if(selectNum == cFlag.getAnswerNumber() ) {
+					System.out.println("正解です！");
+				}else {
+					System.out.println("不正解です...");
+					System.out.println("正解は「" + items[cFlag.getAnswerNumber()] + "」でした。");
+				}
+			}
+		});
+
+		Button nextButton = new Button("次の問題へ");
+		nextButton.setOnAction((event)->{
+			makeFlag(flagBase);
+		});
+
 		root = new BorderPane();
 		root.setTop(flagBase);
-		root.setBottom(decideButton);
+		root.setLeft(flagCombo);
+		root.setCenter(decideButton);
+		root.setRight(nextButton);
+		//root.setBottom(decideButton);
 		Scene scene = new Scene(root);
 		pstage.setScene(scene);
 		pstage.setTitle("Which country is this Flag?");
@@ -42,15 +84,10 @@ public class Main extends Application {
 	}
 
 	void makeFlag(Pane base) {
-		//本来のイメージの縦横比にする方法が解らない
-		//ImagePattern ip = new ImagePattern(new Image("Ex10flag/Data/flag001.png",WIDTH,HEIGHT,true,false));
-		//System.out.println("width  is [" + ip.getWidth() + "]");
-		//System.out.println("height is [" + ip.getHeight() + "]");
-		Countries cFlag = new Countries();
+		cFlag = new Countries();
 		ImagePattern ip = cFlag.getImagePattern();
 		System.out.println("answer is [" + cFlag.getAnswerNumber() + "]");
-		Rectangle rect = new Rectangle(WIDTH * ip.getWidth() ,HEIGHT * ip.getHeight(), ip );
-		//rect.setFill(ip);
+		Rectangle rect = new Rectangle(Countries.WIDTH * ip.getWidth() ,Countries.HEIGHT * ip.getHeight(), ip );
 		base.getChildren().clear();
 		base.getChildren().addAll(rect);
 	}
