1ea0f505bcb2756ffc41137bb2891f2d0bd72eaf
Kaduki-oss <kaduki@pluto.sakura.ne.jp>
2021-03-22 22:04:35 +0900

    継承版完成。一旦commit

---
+++ javaExercise/src/Ex12adapter1/Main.java
@@ -0,0 +1,12 @@
+package Ex12adapter1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Print p = new PrintBanner("Hello");
+		p.printWeak();
+		p.printStrong();
+	}
+
+}
